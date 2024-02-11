1fe7314e9793a2629aeea07a4f4e157e414e181d
= <danpeled31@gmail.com>
2024-02-11 16:12:13 +0200

    yay, the logic was fucked

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,54 +1,52 @@
 import React, { useState, useEffect } from "react";
 
 function HamburgerMenu() {
-	const toBool = (string) => {
-		return string.toLowerCase() === 'true';
-	};
-	let suppousedState = window.localStorage.getItem('menu-open');
-	const [state, setState] = useState(suppousedState == undefined ? false : toBool(suppousedState));
+    const toBool = (string) => {
+        return string.toLowerCase() === 'true';
+    };
+    let suppousedState = window.localStorage.getItem('menu-open');
+    const [state, setState] = useState(suppousedState ? toBool(suppousedState) : false);
 
-	const toggleSidebar = () => {
-		const newState = !state;
-		setState(newState);
-		window.localStorage.setItem('menu-open', newState);
-		renderState(newState);
-	};
+    const toggleSidebar = () => {
+        const newState = !state;
+        setState(newState);
+        window.localStorage.setItem('menu-open', newState.toString());
+    };
 
-	const renderState = (currentState) => {
-		const sidebarList = document.getElementsByClassName('Sidebar')[0];
-		if (sidebarList) {
-			sidebarList.classList.toggle('active', !currentState);
-		}
+    useEffect(() => {
+        const renderState = (currentState) => {
+            const sidebarList = document.getElementsByClassName('Sidebar')[0];
+            if (sidebarList) {
+                sidebarList.classList.toggle('active', !currentState);
+            }
 
-		const elementsToAdjust = [
-			'.page'
-		];
+            const elementsToAdjust = [
+                '.page'
+            ];
 
-		elementsToAdjust.forEach((elementId) => {
-			const element = document.querySelector(elementId);
-			if (element) {
-				if (window.screen.width <= 768) {
-					element.style.marginTop = !currentState ? '360px' : '70px';
-				} else {
-					element.style.marginTop = "0px";
-				}
-			}
-		});
-	};
+            elementsToAdjust.forEach((elementId) => {
+                const element = document.querySelector(elementId);
+                if (element) {
+                    if (window.screen.width <= 768) {
+                        element.style.marginTop = !currentState ? '360px' : '70px';
+                    } else {
+                        element.style.marginTop = "0px";
+                    }
+                }
+            });
+        };
 
+        renderState(state);
+    }, [state]);
 
-	useEffect(() => {
-		renderState(state);
-	}, [state]);
-
-	return (
-		<label className="burger" htmlFor="burger">
-			<input type="checkbox" id="burger" onClick={toggleSidebar}/>
-			<span></span>
-			<span></span>
-			<span></span>
-		</label>
-	);
+    return (
+        <label className="burger" htmlFor="burger">
+            <input type="checkbox" id="burger" checked={!state} onChange={toggleSidebar} />
+            <span></span>
+            <span></span>
+            <span></span>
+        </label>
+    );
 }
 
 export default HamburgerMenu;
